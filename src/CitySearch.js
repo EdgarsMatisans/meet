b25173e2454b7ae61f9addf644204b4f224d0340
Edgars Matisans <80711384+EdgarsMatisans@users.noreply.github.com>
2022-01-17 21:20:21 +0100

    4.3

--- src/CitySearch.js
+++ src/CitySearch.js
@@ -3,11 +3,18 @@ import React, { Component } from "react";
 class CitySearch extends Component {
   state = {
     query: "",
+    suggestions: [],
   };
 
   handleInputChanged = (event) => {
     const value = event.target.value;
-    this.setState({ query: value });
+    const suggestions = this.props.locations.filter((location) => {
+      return location.toUpperCase().indexOf(value.toUpperCase()) > -1;
+    });
+    this.setState({
+      query: value,
+      suggestions,
+    });
   };
 
   render() {
@@ -19,7 +26,14 @@ class CitySearch extends Component {
           value={this.state.query}
           onChange={this.handleInputChanged}
         />
-        <ul className="suggestions"></ul>
+        <ul className="suggestions">
+          {this.state.suggestions.map((suggestion) => (
+            <li key={suggestion}>{suggestion}</li>
+          ))}
+          <li key="all">
+            <b>See all cities</b>
+          </li>
+        </ul>
       </div>
     );
   }
