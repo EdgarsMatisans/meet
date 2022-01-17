f6da68893f24b023f15043738fa3f3aa37607bd8
Edgars Matisans <80711384+EdgarsMatisans@users.noreply.github.com>
2022-01-17 13:11:52 +0100

    4.3

--- src/CitySearch.js
+++ src/CitySearch.js
@@ -1,28 +1,13 @@
-// src/CitySearch.js
-
 import React, { Component } from "react";
 
 class CitySearch extends Component {
   state = {
     query: "",
-    suggestions: [],
   };
 
   handleInputChanged = (event) => {
     const value = event.target.value;
-    const suggestions = this.props.locations.filter((location) => {
-      return location.toUpperCase().indexOf(value.toUpperCase()) > -1;
-    });
-    this.setState({
-      query: value,
-      suggestions,
-    });
-  };
-
-  handleItemClicked = (suggestion) => {
-    this.setState({
-      query: suggestion,
-    });
+    this.setState({ query: value });
   };
 
   render() {
@@ -34,19 +19,7 @@ class CitySearch extends Component {
           value={this.state.query}
           onChange={this.handleInputChanged}
         />
-        <ul className="suggestions">
-          {this.state.suggestions.map((suggestion) => (
-            <li
-              key={suggestion}
-              onClick={() => this.handleItemClicked(suggestion)}
-            >
-              {suggestion}
-            </li>
-          ))}
-          <li>
-            <b>See all cities</b>
-          </li>
-        </ul>
+        <ul className="suggestions"></ul>
       </div>
     );
   }
--- src/EventList.js
+++ src/EventList.js
@@ -4,7 +4,16 @@ import React, { Component } from "react";
 
 class EventList extends Component {
   render() {
-    return <div></div>;
+    const { events } = this.props;
+    return (
+      <ul className="EventList">
+        {events.map((event) => (
+          <li key={event.id}>
+            <Event event={event} />
+          </li>
+        ))}
+      </ul>
+    );
   }
 }
 
